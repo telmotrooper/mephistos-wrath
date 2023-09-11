4434316e9fa2270a0ee9bd09ce453ce18be3fe25
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-11 14:32:42 -0300

    Created node 'AbilityButton'

---
+++ ability_button.gd
@@ -0,0 +1,16 @@
+extends TextureProgressBar
+
+@export var key: StringName
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed(key):
+		print("pressed ", self.name)
