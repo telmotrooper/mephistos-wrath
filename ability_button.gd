0398caabc4b7204fbda5deb2f021ee7b12d4a15b
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-11 19:45:52 -0300

    Added empty ability buttons

--- ability_button.gd
+++ ability_button.gd
@@ -3,23 +3,27 @@ extends TextureProgressBar
 @export var key: StringName
 @export var label: String
 @export var cooldown_time: float = 1.0
+@export var image: CompressedTexture2D
 
 var spinning := false
 var clicked := false
 
 func _ready():
 	$Label.text = label
+	texture_under = image
+	texture_progress = image
 
 func _physics_process(_delta):	
-	if not spinning and (Input.is_action_just_pressed(key) or clicked):
-		spinning = true
-		print("pressed ", self.name)
-		
-		value = 0
-		var tween = create_tween()
-		tween.tween_property(self, "value", 100, cooldown_time)
-		tween.tween_callback(func(): spinning = false)
-	clicked = false
+	if image:
+		if not spinning and (Input.is_action_just_pressed(key) or clicked):
+			spinning = true
+			print("pressed ", self.name)
+			
+			value = 0
+			var tween = create_tween()
+			tween.tween_property(self, "value", 100, cooldown_time)
+			tween.tween_callback(func(): spinning = false)
+		clicked = false
 
 func _gui_input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
