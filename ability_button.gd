389365c0c30e347fe7b78b1986772d8369d84376
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-11 14:45:03 -0300

    Buttons can have different cooldowns

--- ability_button.gd
+++ ability_button.gd
@@ -2,11 +2,19 @@ extends TextureProgressBar
 
 @export var key: StringName
 @export var label: String
+@export var cooldown_time: float = 1.0
+
+var spinning := false
 
 func _ready():
 	$Label.text = label
 
 func _physics_process(_delta):
-	if Input.is_action_just_pressed(key) and not $AnimationPlayer.is_playing():
+	if Input.is_action_just_pressed(key) and not spinning:
+		spinning = true
 		print("pressed ", self.name)
-		$AnimationPlayer.play("rotate")
+		
+		value = 0
+		var tween = create_tween()
+		tween.tween_property(self, "value", 100, cooldown_time)
+		tween.tween_callback(func(): spinning = false)
