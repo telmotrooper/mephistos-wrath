4eb0309babbd46c8b90c841e850dd90239290928
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-11 14:36:57 -0300

    Only press if not playing

--- ability_button.gd
+++ ability_button.gd
@@ -7,5 +7,6 @@ func _ready():
 	$Label.text = label
 
 func _physics_process(_delta):
-	if Input.is_action_just_pressed(key):
+	if Input.is_action_just_pressed(key) and not $AnimationPlayer.is_playing():
 		print("pressed ", self.name)
+		$AnimationPlayer.play("rotate")
