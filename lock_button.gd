02cc0e2af0544f4be59e1a2454b79676ab12187e
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-11 22:32:59 -0300

    Added lock button

---
+++ lock_button.gd
@@ -0,0 +1,13 @@
+extends Button
+
+var locked := false
+
+func _ready() -> void:
+	update_icon()
+
+func _on_pressed() -> void:
+	locked = not locked
+	update_icon()
+
+func update_icon() -> void:
+	icon = load("res://icons/font_awesome/lock.svg") if locked else load("res://icons/font_awesome/unlock.svg")
