b53a8bb31fcdbaf33b0e483b2d0fc20498d81f25
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-11 21:22:48 -0300

    Removed unnecessary conditional

--- ability_button.gd
+++ ability_button.gd
@@ -15,18 +15,17 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	var pressed = Input.is_action_just_pressed("hotkey_" + key) if key else false
-	
-	if ability.image:
-		if not spinning and (pressed or clicked):
-			get_tree().call_group("ability_buttons", "_on_ability_triggered", ability.name)
-			spinning = true
-			print("pressed ", self.name)
-			
-			value = 0
-			var tween = create_tween()
-			tween.tween_property(self, "value", 100, ability.cooldown_time)
-			tween.tween_callback(func(): spinning = false)
-		clicked = false
+
+	if not spinning and (pressed or clicked):
+		get_tree().call_group("ability_buttons", "_on_ability_triggered", ability.name)
+		spinning = true
+		print("pressed ", self.name)
+		
+		value = 0
+		var tween = create_tween()
+		tween.tween_property(self, "value", 100, ability.cooldown_time)
+		tween.tween_callback(func(): spinning = false)
+	clicked = false
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
