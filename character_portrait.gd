d0bd58643c8b0c27faf08db6bae98193c8c1d8d4
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-12 18:46:16 -0300

    Added example tooltip to CharacterPortrait

---
+++ character_portrait.gd
@@ -0,0 +1,29 @@
+@tool
+extends Control
+
+@export_range(0, 50) var health_points = 50 : set = _set_health, get = _get_health
+@export_range(0, 50) var mana_points = 50 : set = _set_mana, get = _get_mana
+
+@onready var hp = $HP
+@onready var mp = $MP
+
+func _set_health(val):
+	if not is_inside_tree():
+		await ready
+	hp.value = val
+
+func _get_health():
+	if not hp:
+		return 50
+	return hp.value
+
+func _set_mana(val):
+	if not is_inside_tree():
+		await ready
+	mp.value = val
+
+func _get_mana():
+	if not mp:
+		return 50
+	else:
+		return mp.value
