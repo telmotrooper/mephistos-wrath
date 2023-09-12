3159e135445f2d515df4f591aec322489ee61d8d
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-12 19:01:18 -0300

    Added character struct

--- character_portrait.gd
+++ character_portrait.gd
@@ -1,11 +1,18 @@
 @tool
 extends Control
 
+@export var character: Character
+
 @export_range(0, 50) var health_points = 50 : set = _set_health, get = _get_health
 @export_range(0, 50) var mana_points = 50 : set = _set_mana, get = _get_mana
 
 @onready var hp = $HP
 @onready var mp = $MP
+
+func _ready() -> void:
+	tooltip_text = "%s\nHP: %d/%d\nMP: %d/%d" % [
+		character.name, character.hp, character.max_hp, character.mp, character.max_mp
+	]
 
 func _set_health(val):
 	if not is_inside_tree():
---
+++ characters/character.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Character
+
+@export var name: StringName = "Character Name"
+
+@export var hp: int
+@export var max_hp: int
+
+@export var mp: int
+@export var max_mp: int
