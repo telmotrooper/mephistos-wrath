a62d6546f6bfef06952d021843a84d6274b8c682
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-13 00:22:28 -0300

    Allow selecting a portrait

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var ability_bar_locked := false
+var selected_characters := []
--- character_portrait.gd
+++ character_portrait.gd
@@ -1,5 +1,7 @@
-@tool
 extends Control
+
+var border : CompressedTexture2D = load("res://icons/player-circle-border.svg")
+var border_selected : CompressedTexture2D = load("res://icons/player-circle-border-selected.svg")
 
 @export var character: Character
 
@@ -9,3 +11,14 @@ func _ready() -> void:
 	]
 	$HP.value = float(character.hp) / character.max_hp * 50
 	$MP.value = float(character.mp) / character.max_mp * 50
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+		GameState.selected_characters = [self]
+		get_tree().call_group("character_portraits", "_on_portrait_selected")
+
+func _on_portrait_selected() -> void:
+	if GameState.selected_characters[0] == self:
+		$Border.texture = border_selected
+	else:
+		$Border.texture = border
