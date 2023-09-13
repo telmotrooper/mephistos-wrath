322674b4d2bdde566728a1533ced26245a298600
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-12 23:47:55 -0300

    Added custom values for each character

--- character_portrait.gd
+++ character_portrait.gd
@@ -3,34 +3,9 @@ extends Control
 
 @export var character: Character
 
-@export_range(0, 50) var health_points = 50 : set = _set_health, get = _get_health
-@export_range(0, 50) var mana_points = 50 : set = _set_mana, get = _get_mana
-
-@onready var hp = $HP
-@onready var mp = $MP
-
 func _ready() -> void:
 	tooltip_text = "%s\nHP: %d/%d\nMP: %d/%d" % [
 		character.name, character.hp, character.max_hp, character.mp, character.max_mp
 	]
-
-func _set_health(val):
-	if not is_inside_tree():
-		await ready
-	hp.value = val
-
-func _get_health():
-	if not hp:
-		return 50
-	return hp.value
-
-func _set_mana(val):
-	if not is_inside_tree():
-		await ready
-	mp.value = val
-
-func _get_mana():
-	if not mp:
-		return 50
-	else:
-		return mp.value
+	$HP.value = float(character.hp) / character.max_hp * 50
+	$MP.value = float(character.mp) / character.max_mp * 50
--- characters/character.gd
+++ characters/character.gd
@@ -3,8 +3,8 @@ class_name Character
 
 @export var name: StringName = "Character Name"
 
-@export var hp: int
 @export var max_hp: int
+@export var hp: int
 
-@export var mp: int
 @export var max_mp: int
+@export var mp: int
