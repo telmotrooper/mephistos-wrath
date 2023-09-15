6af353054249b41278fb431c6fbb4c5e69ae6fed
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-15 17:08:24 -0300

    Added some hotkeys

--- ui.gd
+++ ui.gd
@@ -6,3 +6,15 @@ func _on_top_menu_button_pressed(unique_node_name: String) -> void:
 		node.hide()
 	else:
 		node.show()
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("character_window"):
+		%CharacterWindow.show()
+	elif Input.is_action_just_pressed("abilities_window"):
+		%AbilitiesWindow.show()
+	elif Input.is_action_just_pressed("inventory_window"):
+		%InventoryWindow.show()
+	elif Input.is_action_just_pressed("quest_log_window"):
+		%QuestLogWindow.show()
+	elif Input.is_action_just_pressed("map_window"):
+		%MapWindow.show()
