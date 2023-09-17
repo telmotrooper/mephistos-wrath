1efb3d316efd38c1adb70d835df9deb428886fff
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-17 14:34:17 -0300

    Add decal when character is selected

--- character.gd
+++ character.gd
@@ -1,4 +1,12 @@
 extends Node3D
 
+@export var character: Character
+
 func _ready() -> void:
 	$knight/AnimationPlayer.play("Combat Idle")
+
+func set_selected(value) -> void:
+	$Decal.visible = value
+
+func _on_character_selected() -> void:
+	set_selected(GameState.selected_characters[0].character == character)
