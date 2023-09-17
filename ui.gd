e4f67356efc60faf04c62b07ed32997b8c7c581d
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-17 14:16:26 -0300

    Refactored menu hotkey logic

--- ui.gd
+++ ui.gd
@@ -1,5 +1,13 @@
 extends Control
 
+var action_to_node = {
+	"character_window": "%CharacterWindow",
+	"abilities_window": "%AbilitiesWindow",
+	"inventory_window": "%InventoryWindow",
+	"quest_log_window": "%QuestLogWindow",
+	"map_window": "%MapWindow"
+}
+
 func _on_top_menu_button_pressed(unique_node_name: String) -> void:
 	var node = get_node(unique_node_name)
 	if node.visible:
@@ -8,13 +16,6 @@ func _on_top_menu_button_pressed(unique_node_name: String) -> void:
 		node.show()
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("character_window"):
-		_on_top_menu_button_pressed("%CharacterWindow")
-	elif Input.is_action_just_pressed("abilities_window"):
-		_on_top_menu_button_pressed("%AbilitiesWindow")
-	elif Input.is_action_just_pressed("inventory_window"):
-		_on_top_menu_button_pressed("%InventoryWindow")
-	elif Input.is_action_just_pressed("quest_log_window"):
-		_on_top_menu_button_pressed("%QuestLogWindow")
-	elif Input.is_action_just_pressed("map_window"):
-		_on_top_menu_button_pressed("%MapWindow")
+	for action in action_to_node:
+		if Input.is_action_just_pressed(action):
+			_on_top_menu_button_pressed(action_to_node[action])
