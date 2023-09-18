28a6ee3300c2696c5a656e5816108b183881a668
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-18 20:49:05 -0300

    Detecting click on character

--- character.gd
+++ character.gd
@@ -10,3 +10,7 @@ func set_selected(value) -> void:
 
 func _on_character_selected() -> void:
 	set_selected(GameState.selected_characters[0].character == character)
+
+func _on_character_body_3d_input_event(_camera: Node, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+		print("%s clicked" % character.name)
