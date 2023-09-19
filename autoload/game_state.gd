237398cd5ca94c0f5fb812a365b08da0fde90a69
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-18 23:47:08 -0300

    Ctrl + A selects all party characters

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -5,3 +5,11 @@ var ability_bar_locked := false
 
 var selected_characters: Array[Character] = []
 var active_character: Character
+
+func select_characters(active, selected) -> void:
+	if active != null:
+		GameState.active_character = active
+	GameState.selected_characters.clear()
+	GameState.selected_characters.assign(selected)
+	get_tree().call_group("character_portraits", "_on_portrait_selected")
+	get_tree().call_group("party_characters", "_on_character_selected")
