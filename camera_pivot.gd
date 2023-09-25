9922e49c4a83e8812c0dada7d23b80d82f957c58
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-25 14:09:48 -0300

    Allow marking spot for navigation when paused

--- camera_pivot.gd
+++ camera_pivot.gd
@@ -12,6 +12,8 @@ var min_zoom := 2
 var max_zoom := 5
 var zoom: float
 
+var nav_indicator: Node3D
+
 func _ready() -> void:
 	zoom = %SpringArm3D.spring_length
 
@@ -23,6 +25,9 @@ func _input(event: InputEvent) -> void:
 		zoom -= ZOOM_STEP
 	elif event.is_action_pressed("zoom_out") and zoom < max_zoom:
 		zoom += ZOOM_STEP
+	
+	if GameState.active_character == $"..".character and Input.is_action_just_released("right_mouse_button") and Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
+		navigate()
 
 func _physics_process(delta: float) -> void:
 	vertical = clamp(vertical, -40, 50)
@@ -39,3 +44,23 @@ func grab_camera() -> void:
 	var tween = create_tween()
 	tween.tween_property(GameState.transition_camera, "global_transform:origin", %Camera3D.global_transform.origin, 0.25)
 	tween.tween_callback(func(): %Camera3D.make_current())
+
+func navigate() -> void:
+	var mouse_position = get_viewport().get_mouse_position()
+	var ray_length = 100
+	var from = %Camera3D.project_ray_origin(mouse_position)
+	var to = from + %Camera3D.project_ray_normal(mouse_position) * ray_length
+	var space = get_world_3d().direct_space_state
+	var ray_query = PhysicsRayQueryParameters3D.new()
+	ray_query.set_collision_mask(0b010) # Only collide with layer 2 (floor).
+	ray_query.from = from
+	ray_query.to = to
+	var result = space.intersect_ray(ray_query)
+	if len(result):
+		if is_instance_valid(nav_indicator):
+			nav_indicator.queue_free()
+		%NavigationAgent3D.set_target_position(result.position)
+		nav_indicator = load("res://navigation_indicator.tscn").instantiate()
+		get_parent().add_child(nav_indicator)
+		nav_indicator.global_transform.origin = result.position
+		$"..".navigating = true
