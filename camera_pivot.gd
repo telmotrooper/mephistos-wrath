7695728598de36dec6c7eca23de974d23c23cfa6
Telmo Valverde <telmo.valverde@tarmac.io>
2023-09-25 10:47:57 -0300

    Implemented zoom in/out

--- camera_pivot.gd
+++ camera_pivot.gd
@@ -1,16 +1,25 @@
 extends Node3D
 
-# Camera
 var mouse_sensitivity := 0.5
 var horizontal := 0.0
 var vertical := 0.0
 var h_acceleration := 10.0
 var v_acceleration := 10.0
 
+const ZOOM_STEP := 0.5
+var min_zoom := 2
+var max_zoom := 5
+
 func _input(event: InputEvent) -> void:
+	var zoom = %SpringArm3D.spring_length
+	
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		horizontal -= event.relative.x * mouse_sensitivity
 		vertical += event.relative.y * mouse_sensitivity
+	elif event.is_action_pressed("zoom_in") and zoom > min_zoom:
+		%SpringArm3D.spring_length -= ZOOM_STEP
+	elif event.is_action_pressed("zoom_out") and zoom < max_zoom:
+		%SpringArm3D.spring_length += ZOOM_STEP
 
 func _physics_process(delta: float) -> void:
 	vertical = clamp(vertical, -40, 50)
