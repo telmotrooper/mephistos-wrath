f2520e432ef1e251d6d94b7135f45834d5f545d2
Telmo "Trooper <telmo.valverde@tarmac.io>
2023-11-21 10:26:04 -0300

    Renamed project and scaled UI in 4K (#31)
    * Added hotkey to toggle fullscreen
    
    * Changing UI scale
    
    * Renamed game
    
    * Only scale UI if display is at 4K or higher

---
+++ autoload/hotkeys.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_toggle_fullscreen"):
+		toggle_fullscreen()
+
+func toggle_fullscreen() -> void:
+	get_window().mode = Window.MODE_FULLSCREEN if get_window().mode != Window.MODE_FULLSCREEN else Window.MODE_WINDOWED
